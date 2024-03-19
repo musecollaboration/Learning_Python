b20698bd398d24eb578dc60a74353cc662307192
Aleksandr T <139972964+musecollaboration@users.noreply.github.com>
2024-03-19 21:12:00 +0300

    Update Применение словарей.py

--- Словарь /Применение словарей.py
+++ Словарь /Применение словарей.py
@@ -1,3 +1,6 @@
+# https://www.youtube.com/watch?v=dZ474oWml9k&ab_channel=egoroff_channel
+# https://stepik.org/lesson/296968/step/2?auth=login&unit=278696
+
 # Подсчет количества объектов
 
 # Логика
@@ -40,3 +43,36 @@ for i, j in sorted(d.items()):
     print(i, j)
 
 
+# Разряженный список
+# без словаря
+s = "jhdf HG jgkfYGg jhgkdf 543 *(^$&*#"
+letters = [0] * 26
+for i in s.lower():
+    if i >= "a" and i <= "z":
+        nomer = ord(i) - 97
+        letters[nomer] += 1
+for i in range(26):
+    if letters[i] > 0:
+        print(i, letters[i])
+
+
+# Разряженный список
+# со словарем
+s = "jhdf HG jgkfYGg jhgkdf 543 *(^$&*#"
+d = {}
+for i in s:
+    if i.isalpha():
+        d[i] = d.get(i, 0) + 1
+print(d)
+
+
+# Установление связи между обьектами
+# переводчикб словарь
+words = {}
+while True:
+    s = input()
+    if s in words:
+        print(f'Слово {s} переводится как {words[s]}')
+    else:
+        print('Введите перевод слова')
+        words[s] = input()
