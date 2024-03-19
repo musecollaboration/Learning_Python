b0742410bd6c0209b50c838d7612b3e16bc2b929
Aleksandr T <139972964+musecollaboration@users.noreply.github.com>
2024-03-19 21:54:37 +0300

    Delete Инди-курс программирования на Python/6.4 Словарь. Знакомство.py с типом данных/Задача 1.py

--- Инди-курс программирования на Python/6.4 Словарь. Знакомство.py с типом данных/Задача 1.py
+++
@@ -1,5 +0,0 @@
-# https://stepik.org/lesson/761760/step/3?auth=login&unit=763882
-
-# Создайте пустой словарь и сохраните его в переменную my_dict, затем выведите на экран эту переменную
-my_dict = {}
-print(my_dict)
