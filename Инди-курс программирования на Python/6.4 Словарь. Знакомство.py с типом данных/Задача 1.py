81c851cb1803e9966801f84bab0796f0ab5a16fb
Aleksandr T <139972964+musecollaboration@users.noreply.github.com>
2024-03-19 21:53:17 +0300

    Create Задача 1.py

---
+++ Инди-курс программирования на Python/6.4 Словарь. Знакомство.py с типом данных/Задача 1.py
@@ -0,0 +1,5 @@
+# https://stepik.org/lesson/761760/step/3?auth=login&unit=763882
+
+# Создайте пустой словарь и сохраните его в переменную my_dict, затем выведите на экран эту переменную
+my_dict = {}
+print(my_dict)
