50342d2f39046e07f39fff9e10491adb4d816a41
Aleksandr T <139972964+musecollaboration@users.noreply.github.com>
2024-03-19 21:49:31 +0300

    Create Задача 1.py

---
+++ Инди-курс программирования на Python/6.7 Ситуации, где полезно использовать словарь/Задача 1.py
@@ -0,0 +1,11 @@
+# https://stepik.org/lesson/296968/step/3?auth=login&unit=278696
+
+# Перед вами словарь workers
+# Ваша задача поднять зарплату Бреду до 8500 и затем вывести измененный словарь workers
+
+workers = {
+    'employer1': {'name': 'Jhon', 'salary': 7500},
+    'employer2': {'name': 'Emma', 'salary': 8000},
+    'employer3': {'name': 'Brad', 'salary': 500}
+}
+
