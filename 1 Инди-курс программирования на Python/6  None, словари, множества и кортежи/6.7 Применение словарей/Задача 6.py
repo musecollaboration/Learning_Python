ef73da9ca868213bf147e05d01d24996c53a2aee
Aleksandr T <139972964+musecollaboration@users.noreply.github.com>
2024-03-20 17:55:28 +0300

    Update Задача 6.py

--- 1 Инди-курс программирования на Python/6  None, словари, множества и кортежи/6.7 Применение словарей/Задача 6.py
+++ 1 Инди-курс программирования на Python/6  None, словари, множества и кортежи/6.7 Применение словарей/Задача 6.py
@@ -50,7 +50,7 @@ for i in persons:
 print(data)
 
 # Распаковка кортежа
-Кортежи/Распаковка кортежа.py
+https://github.com/musecollaboration/Learning_Python/blob/main/%D0%9A%D0%BE%D1%80%D1%82%D0%B5%D0%B6%D0%B8/%D0%A0%D0%B0%D1%81%D0%BF%D0%B0%D0%BA%D0%BE%D0%B2%D0%BA%D0%B0%20%D0%BA%D0%BE%D1%80%D1%82%D0%B5%D0%B6%D0%B0.py
 
 # s = ('Allison Hill', 334053, 'M', '1635644202')
 # name, salary, gender, passport = s
